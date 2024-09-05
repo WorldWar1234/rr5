430f4dc8f935879a4b2abeffeb64a07b0d123973
Rushs321 <fyggh566@tutamail.com>
2024-09-05 16:22:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import fastify from 'fastify';
 import { processRequest } from './src/proxy.js'; // Import the named export
 
 const app = fastify({ 
-  logger: true
+  logger: false
 });
 
 const PORT = process.env.PORT || 8080;
